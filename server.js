3846199fb742ed16f135745cdca9af69ed4141a1
siddarthsharma <sid9940@gmail.com>
2017-08-08 15:17:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
 app.get('/article-one',function(req,res){
    res.send("article one is requested here !!!!"); 
 });
@@ -24,6 +20,12 @@ app.get('/article-two',function(req,res){
 app.get('/article-three',function(req,res){
    res.send("article three is requested here !!!!"); 
 });
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
